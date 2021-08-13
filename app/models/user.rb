4e0ab6cec283d6aff8ae5780686799d045a1eda7
victor jara <chogo01@hotmail.com>
2021-08-13 10:49:47 -0400

    Configuración completa para empezar el proyecto

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   validates :email, format: { with: /\A.*@.*\.com\z/ }, presence: true, uniqueness: true
+  has_one_attached :photo
 end
