a1b9c2e8146e93d3304e50e042ed782b7e8a973c
victor jara <chogo01@hotmail.com>
2021-08-13 09:56:03 -0400

    Se completan las validaciones de todos modelos

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,7 @@
 class Service < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :category, presence: true, inclusion: { in: ["gasfiteria", "electricidad", "pintura", "carpinteria", "servicio técnico", "limpieza"] }
+  validates :price, presence: true
 end
