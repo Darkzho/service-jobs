9416f92e931360ada114e18e8af8511d49acd0e3
victor jara <chogo01@hotmail.com>
2021-08-17 12:58:27 -0400

    armando booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,12 +2,28 @@ class BookingsController < ApplicationController
   def index
   end
   
-  def new   
+  def new
+    @booking = Booking.new
+    authorize @booking   
   end
 
   def create 
+    @service = Service.find(params[:service_id])
+    @booking = Booking.new(booking_param)
+    @booking.service = @service
+    @booking.user = current_user
+    authorize @booking
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :edit
+    end
   end
   
   def show
+  end
+
+  def booking_param
+    params.require(:booking).permit(:start_date, :end_date, :service_id, :user_id)
   end
 end
