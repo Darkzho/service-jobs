1848c87ce1fc6e59475493e5ca326d96c6b1777e
victor jara <chogo01@hotmail.com>
2021-08-23 20:06:47 -0400

    Se agrega promedio de reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,4 @@
 class UsersController < ApplicationController
-  def index
-    @user = current_user
-  end
-  
-  def menu
-    
-  end
-
   def show
     @users = User.geocoded
 
@@ -17,6 +9,7 @@ class UsersController < ApplicationController
         info_window: render_to_string(partial: "info_window", locals: { user: user })
       }
     end
+
     @user = User.find(params[:id])
     @services = @user.services
     authorize @user
