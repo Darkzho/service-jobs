6aa75d2b840319520b82729f3cf53afa8c3ff0a3
EmmanueleTorres <emmanueletorres@gmail.com>
2021-08-26 12:11:23 -0400

    mapa arreglado

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,9 @@
 class UsersController < ApplicationController
   def show
-    
     @user = User.find(params[:id])
     @users = User.geocoded
 
-    @markers = @users.geocoded.map do |user|
-      if user == @user
-        {
-          lat: user.latitude,
-          lng: user.longitude,
-          info_window: render_to_string(partial: "info_window", locals: { user: user })
-        }
-      end
-    end
+    @markers = { lat: @user.geocode[0], lng: @user.geocode[1] }
     @services = @user.services
     authorize @user
   end 
