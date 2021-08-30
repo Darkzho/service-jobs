a5373b036dcd2db7d3ee1f8c9bc0a8cb9f685fa3
EmmanueleTorres <emmanueletorres@gmail.com>
2021-08-30 18:36:49 -0400

    mejorado formulario booking

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,5 +1,8 @@
 import flatpickr from "flatpickr";
 import { Spanish } from "flatpickr/dist/l10n/es.js"
+const today = new Date()
+const tomorrow = new Date(today)
+tomorrow.setDate(tomorrow.getDate() + 1)
 
 const initFlatpickr = () => {
   flatpickr(".datepicker", {
@@ -7,7 +10,7 @@ const initFlatpickr = () => {
     altFormat: "j F Y, h:i K",
     enableTime: true,
     dateFormat: "Y-m-d H:i:S",
-    minDate: "today",
+    minDate : tomorrow,
     "locale": Spanish
   });
 }
